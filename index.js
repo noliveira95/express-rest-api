086d596c5e92d5bd1dd5e6cfe07b0666e6c5d199
Nick Oliveira <87996659+noliveira95@users.noreply.github.com>
2022-06-18 20:12:21 -0700

    Added put req, refactored validation

--- index.js
+++ index.js
@@ -28,21 +28,35 @@ app.get("/api/courses/:id", (req, res) => {
 });
 
 app.post("/api/courses", (req, res) => {
-  const schema = Joi.object({
-    id: Joi.number(),
-    name: Joi.string().min(3).required(),
-  });
   const course = {
     id: courses.length + 1,
     name: req.body.name,
   };
-  const result = schema.validate(course);
-
-  if (result.error) {
-    res.status(400).send(result.error.details[0].message);
+  const { error } = validate(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
     return;
   }
   courses.push(course);
+  res.send(course);
+});
+
+app.put("/api/courses/:id", (req, res) => {
+  // Look up course
+  // If not found, return 404 - Not found
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) {
+    return res.status(404).send("Course was not found");
+  }
+  // Validate
+  // If invalid, return 400 - Bad request
+  const { error } = validate(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+  // Update course
+  // Return updated course to client
+  course.name = req.body.name;
   res.send(course);
 });
 
@@ -51,3 +65,12 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
 });
+
+// VALIDATE COURSE
+const validate = (course) => {
+  const schema = Joi.object({
+    id: Joi.number(),
+    name: Joi.string().min(3).required(),
+  });
+  return schema.validate(course);
+};
